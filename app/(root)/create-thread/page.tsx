103cee4b0b7ea7898eeef4ea88f3008651af5a48
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-10-06 17:46:37 +0200

    add create thread page

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,25 @@
+import { fetchUser } from "@/lib/actions/user.actions";
+import PostThread from "@/components/forms/postThread";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+async function Page() {
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if(!userInfo?.onboarded) redirect("/onboarding");
+
+    return (
+        <>
+            <h1 className="head-text">Skapa tråd</h1>
+
+            <PostThread userId={userInfo._id} />
+        </>
+        
+    );
+}
+
+export default Page;
---
+++ lib/validations/thread.ts
@@ -0,0 +1,11 @@
+import * as z from "zod"
+
+export const threadValidation = z.object({
+    thread: z.string().nonempty().min(3, { message: "Thread must be at least 3 characters long"}).max(40, { message: "Thread must be at most 30 characters long" }),
+    accountId: z.string(),
+})
+
+export const commentValidation = z.object({
+    thread: z.string().nonempty().min(3, { message: "Thread must be at least 3 characters long"}).max(40, { message: "Thread must be at most 30 characters long" }),
+    accountId: z.string(),
+})
