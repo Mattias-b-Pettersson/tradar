c1b4050ddc706cdfd67acbc2bbd5f3444c03291e
Mattias-b-Pettersson <104089548+Mattias-b-Pettersson@users.noreply.github.com>
2023-10-06 17:47:52 +0200

    update form label

--- components/forms/postThread.tsx
+++ components/forms/postThread.tsx
@@ -63,7 +63,7 @@ function postThread({userId}: Props) {
                     render={({ field }) => (
                         <FormItem className='flex w-full flex-col gap-3'>
                         <FormLabel className='text-base-semibold text-light-2'>
-                            Content
+                            Innehåll
                         </FormLabel>
                         <FormControl className='no-focus border-dark-4 bg-dark-3 text-light-1'>
                             <Textarea
